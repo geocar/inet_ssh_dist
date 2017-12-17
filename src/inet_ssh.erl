a6a26e0b57ea1cdec113da1f061df08943545574
Geo Carncross <geocar@gmail.com>
2017-12-17 09:44:22 +0000

    packet support

--- src/inet_ssh.erl
+++ src/inet_ssh.erl
@@ -3,12 +3,17 @@
 -export([getserv/1, getaddr/1, getaddr/2, getaddrs/1, getaddrs/2]).
 -export([connect/3, connect/4, listen/2, accept/1, accept/2, close/1]).
 -export([send/2, send/3, recv/2, recv/3, shutdown/2]).
+-export([ll/1, setopts/2]).
 
 -record(inet_ssh, { pid, ref }).
 
 getserv(X) -> inet_tcp:getserv(X).
 getaddr(X) -> inet_tcp:getaddr(X).    getaddr(X,Timer) -> inet_tcp:getaddr(X,Timer).
 getaddrs(X) -> inet_tcp:getaddrs(X).  getaddrs(X,Timer) -> inet_tcp:getaddrs(X,Timer).
+
+ll(#inet_ssh{ pid = Pid }) -> gen_server:call(Pid, ll); ll(Socket) -> Socket.
+
+setopts(#inet_ssh{ pid = Pid, ref = Ref }, Opts) -> gen_server:cast(Pid, {set_options, Ref, Opts}); setopts(Socket, Opts) -> inet:setopts(Socket, Opts).
 
 connect(Address, Port, Opts) -> connect(Address, Port, Opts, infinity).
 connect(Address, Port, Opts, Timeout) ->
@@ -26,9 +31,12 @@ accept(X) -> inet_tcp:accept(X).  accept(X,T) -> inet_tcp:accept(X,T).
 close(X) -> shutdown(X, read_write).
 
 send(Socket, Packet) -> send(Socket, Packet, []).
-send(#inet_ssh{ pid = Pid, ref = Ref }, Packet, Opts) -> gen_server:cast(Pid, {send, Ref, Packet, Opts}).
+send(#inet_ssh{ pid = Pid, ref = Ref }, Packet, Opts) -> gen_server:cast(Pid, {send, Ref, Packet, Opts});
+send(Socket, Packet, Opts) -> inet_tcp:send(Socket, Packet, Opts).
 
 recv(Socket, Length) -> recv(Socket, Length, infinity).
-recv(#inet_ssh{ pid = Pid, ref = Ref }, Length, Timeout) -> gen_server:call(Pid, {recv, Ref, Length}, Timeout).
+recv(#inet_ssh{ pid = Pid, ref = Ref }, Length, Timeout) -> gen_server:call(Pid, {recv, Ref, Length}, Timeout);
+recv(Socket, Length, Timeout) -> inet_tcp:recv(Socket, Length, Timeout).
 
-shutdown(#inet_ssh{ pid = Pid, ref = Ref }, How) -> gen_server:cast(Pid, {close, Ref, How}), ok.
+shutdown(#inet_ssh{ pid = Pid, ref = Ref }, How) -> gen_server:cast(Pid, {close, Ref, How}), ok;
+shutdown(Socket, How) -> inet_tcp:shutdown(Socket, How).
--- src/inet_ssh_backend.erl
+++ src/inet_ssh_backend.erl
@@ -3,7 +3,7 @@
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]). %gen_server
 -record(state, { ssh, chan }).
--record(chan, { buffer = <<>>, eof_write = false, eof_read = false, sleepers = [] }).
+-record(chan, { recv_list = false, packet = 0, buffer = <<>>, eof_write = false, eof_read = false, sleepers = [] }).
 
 -define(PACKET_SIZE, 32768).
 -define(WINDOW_SIZE, 4*?PACKET_SIZE).
@@ -14,11 +14,15 @@ init([Address]) ->
 	{ok, SSH} = ssh:connect(Host, 22, Opts),
 	{ok, #state{ ssh = SSH, chan = #{} }}.
 
-handle_call({connect, Port, _Opts}, _From, State = #state{ chan = Channels, ssh = SSH }) ->
+handle_call({connect, Port, Opts}, _From, State = #state{ chan = Channels, ssh = SSH }) ->
 	H = <<"localhost">>, L=byte_size(H), P = 10000 + rand:uniform(55000),
 	M = <<L:32/big, H/binary, Port:32/big, L:32/big, H/binary, P:32/big>>,
 	{open, Ref} = ssh_connection_handler:open_channel(SSH, "direct-tcpip", M, ?WINDOW_SIZE, ?PACKET_SIZE, infinity),
-	{reply, Ref, State#state{ chan = maps:put(Ref, #chan{}, Channels) } };
+	%io:fwrite(standard_error, "open channel ~p (port ~p)~n", [Ref,Port]),
+	{reply, Ref, State#state{ chan = maps:put(Ref, set_options(#chan{}, Opts), Channels) } };
+handle_call(ll, _From, State = #state{ ssh = SSH }) ->
+	[{socket,Sock}] = ssh:connection_info(SSH,[socket]),
+	{reply, Sock, State};
 handle_call({recv, Ref, Length}, From, State) ->
 	self() ! {wait, From, Ref, Length},
 	{noreply, State}.
@@ -40,24 +44,61 @@ handle_cast({close, Ref, read}, State = #state{ chan = Channels }) ->
 handle_cast({close, Ref, read_write}, State = #state{ ssh = SSH, chan = Channels }) ->
 	ssh_connection:close(SSH, Ref),
 	{noreply, State#state{ chan = maps:remove(Ref, Channels) } };
-handle_cast({send, Ref, Packet, _Opts}, State = #state{ ssh = SSH }) -> ssh_connection:send(SSH, Ref, Packet), {noreply, State};
+handle_cast({set_options, Ref, Opts}, State) ->
+	{noreply, with_chan(fun(C) -> set_options(C, Opts) end, Ref, State)};
+handle_cast({send, Ref, Data, _Opts}, State = #state{ ssh = SSH, chan = Channels }) ->
+	#chan{ packet = P } = maps:get(Ref, Channels),
+	Packet = encode_packet(P, iolist_to_binary(Data)),
+	%io:fwrite(standard_error, "~p send ~p~n", [Ref,Packet]),
+	ssh_connection:send(SSH, Ref, Packet), {noreply, State};
 handle_cast(_, State) -> {noreply, State}.
 
-handle_info({ssh_cm, SSH, {data, Ref, _, Data}}, State = #state{ ssh = SSH, chan = Channels }) ->
-	C = #chan{ buffer = B, sleepers = W } = maps:get(Ref, Channels), wake(W),
-	{noreply, State#state{ chan = maps:put(Ref, C#chan{ buffer = <<B/binary, Data/binary>>, sleepers = [] }, Channels) }};
-handle_info({ssh_cm, SSH, {eof, Ref}}, State = #state{ ssh = SSH, chan = Channels }) ->
-	C = #chan{ sleepers = W } = maps:get(Ref, Channels), wake(W),
-	{noreply, State#state{ chan = maps:put(Ref, C#chan{ eof_read = true, sleepers = [] }, Channels) }};
+set_options(C = #chan{}, Opts) ->
+	P = proplists:get_value(packet, Opts, 0),
+	LM = lists:member(list, Opts),
+	C#chan{ packet = P, recv_list = LM }.
+
+encode_packet(1, D) -> DL = byte_size(D), <<DL, D/binary>>;
+encode_packet(2, D) -> DL = byte_size(D), <<DL:16/big, D/binary>>;
+encode_packet(4, D) -> DL = byte_size(D), <<DL:32/big, D/binary>>;
+encode_packet(_, D) -> D. % raw, 0
+
+with_chan(F, Ref, State = #state{ chan = Channels }) ->
+	case maps:get(Ref, Channels, undefined) of undefined -> State; C -> State#state{ chan = maps:put(Ref, F(C), Channels) } end.
+
+with_chan_data(F, Ref, State) ->
+	with_chan(fun(C = #chan{ sleepers = W }) ->
+		P = self(), lists:foreach(fun(X) -> P ! X end, W),
+		F(C#chan{ sleepers = [] })
+	end, Ref, State).
+
+xlate(#chan{ recv_list = true }, X) -> binary_to_list(X);
+xlate(#chan{ recv_list = false }, X) -> X.
+
+handle_info({ssh_cm, SSH, {data, Ref, _, Data}}, State = #state{ ssh = SSH }) ->
+	%io:fwrite("~p data ~p~n",[Ref, Data]),
+	{noreply, with_chan_data(fun(C = #chan{ buffer = B }) -> C#chan{ buffer = <<B/binary, Data/binary>> } end, Ref, State)};
+handle_info({ssh_cm, SSH, {eof, Ref}}, State = #state{ ssh = SSH }) ->
+	%io:fwrite("~p eof~n",[Ref]),
+	{noreply, with_chan_data(fun(C) -> C#chan{ eof_read = true } end, Ref, State)};
 handle_info(X = {wait,From,Ref,Length}, State = #state{ chan = Channels }) ->
-	C = #chan{ buffer = B, sleepers = W } = maps:get(Ref, Channels),
+	C = #chan{ buffer = B, sleepers = W, packet = P } = maps:get(Ref, Channels),
 	C2 = if
+		P =/= 0, C#chan.eof_read ->
+			gen_server:reply(From, {error, closed}),
+			C;
+		P =/= 0 ->
+			case erlang:decode_packet(P, B, []) of
+				{ok, Left, Right} -> gen_server:reply(From, {ok, xlate(C, Left)}), C#chan{ buffer = Right };
+				{more, _Needmore} -> C#chan{ sleepers = [X|W] };
+				E = {error, _Reason} -> gen_server:reply(From, E), C
+			end;
 		byte_size(B) >= Length ->
 			Left = binary:part(B, 0, Length), Right = binary:part(B, Length, byte_size(B) - Length),
-			gen_server:reply(From, {ok, Left}),
+			gen_server:reply(From, {ok, xlate(C, Left)}),
 			C#chan{ buffer = Right };
 		C#chan.eof_read, byte_size(B) > 0 ->
-			gen_server:reply(From, {ok, B}),
+			gen_server:reply(From, {ok, xlate(C, B)}),
 			C#chan{ buffer = <<>> };
 		C#chan.eof_read ->
 			gen_server:reply(From, {error, closed}),
@@ -66,10 +107,8 @@ handle_info(X = {wait,From,Ref,Length}, State = #state{ chan = Channels }) ->
 			C#chan{ sleepers = [X|W] }
 	end,
 	{noreply, State#state{ chan = maps:put(Ref, C2, Channels) } };
-handle_info(Else, State) ->
+handle_info(_Else, State) ->
 	{noreply, State}.
-
-wake(W) -> P = self(), lists:foreach(fun(X) -> P ! X end, W).
 
 code_change(_Old, State, _Extra) -> {ok, State}.
 terminate(_Reason, #state{ ssh = SSH, chan = Channels }) -> 
